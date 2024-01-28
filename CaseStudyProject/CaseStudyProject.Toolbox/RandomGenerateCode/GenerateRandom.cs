e0ec8aa184386da9e8a13f3d8a4122e35894427c
nergu <nergulkahya019@gmail.com>
2024-01-28 13:50:33 +0300

    alternatif guid code üretme fonksiyonu yazıldı.

--- CaseStudyProject/CaseStudyProject.Toolbox/RandomGenerateCode/GenerateRandom.cs
+++ CaseStudyProject/CaseStudyProject.Toolbox/RandomGenerateCode/GenerateRandom.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MiNET.Utils;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CaseStudyProject.Toolbox.RandomGenerateCode
 {
@@ -29,18 +32,9 @@ namespace CaseStudyProject.Toolbox.RandomGenerateCode
                 {
                     randomCodeCharList2.Add(randomCode);
                 }
-                #region verify fonksiyonu ile benzer şekilde çalışır biri char kontrol ederken biri string olarak kontrol eder...
-                if (!randomCodeCharList.Contains(randomCode))
-                {
-                    //Liste elemanlarından biri yeni üretilen code ile aynı olup oladıgını kontrol eder ve eğer aynı değilse 'randomCodeCharListine' ekler.
-                    randomCodeCharList.Add(randomCode);
-                    //randomCode = string.Empty;
-                }
-                #endregion
             }
             return randomCodeCharList;
         }
-
         private static bool RandomCodeVerify(List<string> randomCodeCharList, string randomCode)
         {
             if (randomCodeCharList.Count > 0)
@@ -71,6 +65,43 @@ namespace CaseStudyProject.Toolbox.RandomGenerateCode
             }
             return true;
         }
+
+        #region alternatif
+        public static List<string> GenerateRandomCode2()
+        {
+
+            string charList = "ACDEFGHKLMNPRTXYZ234579";
+            List<string> randomCodeCharList = new List<string>();
+            List<string> randomCodeCharList2 = new List<string>();
+            Random random = new Random();
+            int guidSayac = 0;
+            while (randomCodeCharList.Count < 1000)
+            {
+                var guid = Guid.NewGuid().ToString("N").ToUpper();
+                string randomCode = string.Empty;
+
+                while (randomCode.Length < 8)
+                {
+
+                    if (charList.Contains(guid[guidSayac]))
+                    {
+                        randomCode = randomCode + guid[guidSayac];
+
+                    }
+                    guidSayac++;
+                }
+                #region verify fonksiyonu ile benzer şekilde çalışır biri char kontrol ederken biri string olarak kontrol eder...
+                if (!randomCodeCharList.Contains(randomCode))
+                {
+                    randomCodeCharList.Add(randomCode);
+                    randomCode = string.Empty;
+                }
+                #endregion
+            }
+            return randomCodeCharList;
+        }
+
+        #endregion
     }
 }
 
